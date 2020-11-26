142cd1511d7e39bc16d586d8967b98f27833d342
Bartek Kocięcki <bartosz.kociecki@gmail.com>
2020-11-27 00:22:33 +0100

    update

--- Resources/Beginner/Day_3/ticket_photo.py
+++ Resources/Beginner/Day_3/ticket_photo.py
@@ -3,20 +3,25 @@
 print("Wlcome to the rollercoaster: ")
 
 height = int(input("What is your height in cm?: "))
+bill = 0
 
 if height >= 120:
     #print("You may get on!")
     age = int(input("How old are you?: "))
     if age < 12:
+        bill = 5
         print("5 dollars, please")
     elif age > 12 and age < 18:
+        bill = 7
         print("7 dollars, please")
     else:
+        bill = 12
         print("12 dollars, please ")
 
-    ans = input("Do you want a photo taken. Y or N. ")
-    if ans == 'Y':
-        print("Added $3")
+    wants_photo = input("Do you want a photo taken. Y or N. ")
+    if wants_photo == 'Y':
+        bill += 3
+        print(f"{bill} dollars, please ")
     else:
         print("You can go!")
 else:
