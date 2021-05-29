f601e5c22d4c3615beeab2cd2c8ff521a3ddb9d5
portnojek <bartosz.kociecki@gmail.com>
2021-05-29 13:33:30 +0200

    commit

--- Resources/Beginner/Day_8/prime_numbers.py
+++ Resources/Beginner/Day_8/prime_numbers.py
@@ -1,11 +1,14 @@
+
+liczba = int(input("Wpisz interesującą Cię liczbę..."))
+
 def prime_checker(number):
     is_prime = True
     for i in range(2, number):
         if number % i == 0:
             is_prime = False
     if is_prime:
-        print("To jest liczba prime")
+        print("To jest liczba pierwsza")
     else:
-        print("To nie jest liczba prime")
+        print("To nie jest liczba pierwsza")
 
-prime_checker(4)
+prime_checker(liczba)
