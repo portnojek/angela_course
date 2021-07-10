6c469353746e921d0b2f147967cda92445645798
portnojek <bartosz.kociecki@gmail.com>
2021-07-10 22:44:10 +0200

    ddd

--- PNL/przetwarzanie_XML.py
+++ PNL/przetwarzanie_XML.py
@@ -30,3 +30,35 @@ d=et.parse("/home/bartek/Dokumenty/Python/angela_course/PNL/dane.xml")
 root=d.getroot()
 for e in root.findall('jezyki'): # tylko po elementach "jezyki"
     print(e.text)
+
+#więcej niż jeden element
+import xml.etree.ElementTree as et
+d=et.parse("/home/bartek/Dokumenty/Python/angela_course/PNL/dane.xml")
+root=d.getroot()
+print (root.findall('jezyki')[1].text)
+
+#atrybuty
+import xml.etree.ElementTree as et
+tree=et.parse("/home/bartek/Dokumenty/Python/angela_course/PNL/dane.xml")
+root=tree.getroot()
+nazwisko=root.find('nazwisko')
+print(nazwisko.attrib)
+
+import xml.etree.ElementTree as et
+tree=et.parse("/home/bartek/Dokumenty/Python/angela_course/PNL/dane.xml")
+root=tree.getroot()
+nazwisko=root.find('nazwisko')
+print(nazwisko.attrib['param'])
+
+#lub krócej
+import xml.etree.ElementTree as et
+print(et.parse("/home/bartek/Dokumenty/Python/angela_course/PNL/dane.xml").getroot().find("nazwisko").attrib['param'])
+
+#SZTUCZKI
+#odczytywanie XML jako zwykły tekst - tostring
+import xml.etree.ElementTree as et
+drzewko= et.parse("/home/bartek/Dokumenty/Python/angela_course/PNL/dane.xml")
+korzen = drzewko.getroot()
+print(et.tostring(korzen))
+
+#sprawdzanie nazwy elementu
